661cb8ed7e57673f63725baa0ce90454c17244bb
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2017-03-19 14:27:59 +0100

    test with multiple targets

---
+++ Assets/setActiveSounds.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Vuforia;
+
+public class setActiveSounds : MonoBehaviour {
+
+    private AudioSource otherAudio;
+    // Use this for initialization
+    void Start () {
+        otherAudio = GameObject.Find("Test").GetComponent<AudioSource>();
+        Debug.Log(otherAudio);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Get the Vuforia StateManager
+        StateManager sm = TrackerManager.Instance.GetStateManager();
+
+        // Query the StateManager to retrieve the list of
+        // currently 'active' trackables 
+        //(i.e. the ones currently being tracked by Vuforia)
+        IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours();
+
+        if (!otherAudio.isPlaying)
+        {
+            otherAudio.Play();
+            Debug.Log("ok");
+        }
+
+        // Iterate through the list of active trackables
+        Debug.Log("List of trackables currently active (tracked): ");
+        foreach (TrackableBehaviour tb in activeTrackables)
+        {
+            Debug.Log("Trackable: " + tb.TrackableName);
+        }
+    }
+}
