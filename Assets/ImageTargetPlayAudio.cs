5948fe64642a42b52a39461017590d32e849c5b6
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2017-03-20 09:43:52 +0100

    no message

---
+++ Assets/ImageTargetPlayAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetPlayAudio : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+ 	private AudioSource audio;
+    void Start()
+    {
+    	audio = GetComponent<AudioSource>();
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+     
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            audio.Play();
+        }
+        else
+        {
+            // Stop audio when target is lost
+            audio.Stop();
+        }
+    }   
+}
---
+++ Assets/TrackableList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia; 
+
+public class TrackableList : MonoBehaviour {
+
+	AudioClip clips;
+	// Use this for initialization
+	void Start () {
+		clips = new AudioClip[6];
+    	clips[0] = Resources.Load(Resources.Load("AudioSourcePrefab",GameObject).GetComponent(AudioSource));  //  First clip
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Get the Vuforia StateManager
+        StateManager sm = TrackerManager.Instance.GetStateManager ();
+ 
+        // Query the StateManager to retrieve the list of
+        // currently 'active' trackables 
+        //(i.e. the ones currently being tracked by Vuforia)
+        IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours ();
+ 
+       // Iterate through the list of active trackables
+        Debug.Log ("List of trackables currently active (tracked): ");
+        foreach (TrackableBehaviour tb in activeTrackables) {
+            Debug.Log("Trackable: " + tb.TrackableName);
+        }
+	}
+}
